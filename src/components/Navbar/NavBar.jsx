55c13faa00fe95e76206d20a18a621c87bca7859
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-03 20:29:11 +0100

    Feat: Create a header.

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { css } from "@emotion/react";
+import { container, title, icon, span } from "./NavBar.style";
 
 const NavBar = () => {
   return (
     <>
-      <h2>NavBar</h2>
+      <div css={container}>
+        <i css={icon} className="fa-brands fa-sellsy"></i>
+        <h1 css={title}>
+          Campaign <span css={span}>Manager</span>
+        </h1>
+      </div>
     </>
   );
 };
