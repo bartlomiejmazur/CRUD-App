9077fdb4f1faf88e78ccbfffefb0cc92a3345ecf
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-03 11:19:46 +0100

    Add: Paths for switching between components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import NavBar from "./components/Navbar/NavBar";
+import CampaignList from "./components/campaigns/CampaignList/CampaignList";
+import AddCampaign from "./components/campaigns/AddCampaign/AddCampaign";
+import ViewCampaign from "./components/campaigns/ViewCampaign/ViewCampaign";
+import EditCampaign from "./components/campaigns/EditCampaign/EditCampaign";
 
 function App() {
   return (
     <div>
-      <h1> Hello React </h1>
+      <NavBar />
+      <Routes>
+        <Route path={"/"} element={<Navigate to={"/campaigns/list"} />} />
+        <Route path={"/campaigns/list"} element={<CampaignList />} />
+        <Route path={"/campaigns/add"} element={<AddCampaign />} />
+        <Route
+          path={"/campaigns/view/:campaignId"}
+          element={<ViewCampaign />}
+        />
+        <Route
+          path={"/campaigns/view/:campaignId"}
+          element={<EditCampaign />}
+        />
+      </Routes>
     </div>
   );
 }
