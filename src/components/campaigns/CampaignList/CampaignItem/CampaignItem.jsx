7e5ef0a72d07b6ea8e8cca2cc9c98e1998b9a45a
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-04 20:49:57 +0100

    Feat: Displaying data information.

--- src/components/campaigns/CampaignList/CampaignItem/CampaignItem.jsx
+++ src/components/campaigns/CampaignList/CampaignItem/CampaignItem.jsx
@@ -12,28 +12,34 @@ import {
 import { css } from "@emotion/react";
 import { Link } from "react-router-dom";
 
-const CampaignItem = () => {
-  return (
-    <div css={container}>
-      <div css={itemTitle}>Title</div>
-      <div css={itemBid}>Bid amout:</div>
-      <div css={itemStatus}>Status:</div>
-      <div css={itemTown}>Town:</div>
-      <CircleButton variant="view">
-        <Link to={`/campaigns/view/:campaignId`}>
-          <i css={{ color: "white" }} className="fa-solid fa-eye"></i>
-        </Link>
-      </CircleButton>
-      <CircleButton variant="edit">
-        <Link to={`/campaigns/edit/:campaignId`}>
-          <i css={{ color: "white" }} className="fa-solid fa-pen-to-square"></i>
-        </Link>
-      </CircleButton>
-      <CircleButton variant="delete">
-        <i className="fa-solid fa-trash"></i>
-      </CircleButton>
-    </div>
-  );
+const CampaignItem = ({ data }) => {
+  return data.map((el) => {
+    return (
+      <div css={container} key={el.id}>
+        <div css={itemTitle}>{el.name}</div>
+        <div css={itemBid}>Bid amout: {el.bid_amount}</div>
+        <div css={itemStatus}>Status: {el.status ? "On" : "Off"}</div>
+        <div css={itemTown}>Town: {el.town}</div>
+
+        <CircleButton variant="view">
+          <Link to={`/campaigns/view/:campaignId`}>
+            <i css={{ color: "white" }} className="fa-solid fa-eye"></i>
+          </Link>
+        </CircleButton>
+        <CircleButton variant="edit">
+          <Link to={`/campaigns/edit/:campaignId`}>
+            <i
+              css={{ color: "white" }}
+              className="fa-solid fa-pen-to-square"
+            ></i>
+          </Link>
+        </CircleButton>
+        <CircleButton variant="delete">
+          <i className="fa-solid fa-trash"></i>
+        </CircleButton>
+      </div>
+    );
+  });
 };
 
 export default CampaignItem;
