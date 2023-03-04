3d2aa418f18dc2d4e34f85068a74c1698d16043a
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-04 20:44:04 +0100

    Feat: Conect db & fetch data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+
 import NavBar from "./components/Navbar/NavBar";
 import CampaignList from "./components/campaigns/CampaignList/CampaignList";
 import AddCampaign from "./components/campaigns/AddCampaign/AddCampaign";
@@ -8,12 +11,27 @@ import ViewCampaign from "./components/campaigns/ViewCampaign/ViewCampaign";
 import EditCampaign from "./components/campaigns/EditCampaign/EditCampaign";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const dataCollectionRef = collection(db, "campaigns");
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const dataDb = await getDocs(dataCollectionRef);
+      setData(dataDb.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getData();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path={"/"} element={<Navigate to={"/campaigns/list"} />} />
-        <Route path={"/campaigns/list"} element={<CampaignList />} />
+        <Route
+          path={"/campaigns/list"}
+          element={<CampaignList data={data} />}
+        />
         <Route path={"/campaigns/add"} element={<AddCampaign />} />
         <Route
           path={"/campaigns/view/:campaignId"}
