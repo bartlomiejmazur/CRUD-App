042c4f3fea0c5052186384c195544c7ef44b5c99
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-05 12:19:18 +0100

    Delete: Rmove fetch data state to CampaignList.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
-
 import NavBar from "./components/Navbar/NavBar";
 import CampaignList from "./components/campaigns/CampaignList/CampaignList";
 import AddCampaign from "./components/campaigns/AddCampaign/AddCampaign";
@@ -11,27 +8,12 @@ import ViewCampaign from "./components/campaigns/ViewCampaign/ViewCampaign";
 import EditCampaign from "./components/campaigns/EditCampaign/EditCampaign";
 
 function App() {
-  const [data, setData] = React.useState([]);
-  const dataCollectionRef = collection(db, "campaigns");
-
-  React.useEffect(() => {
-    const getData = async () => {
-      const dataDb = await getDocs(dataCollectionRef);
-      setData(dataDb.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getData();
-  }, []);
-
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path={"/"} element={<Navigate to={"/campaigns/list"} />} />
-        <Route
-          path={"/campaigns/list"}
-          element={<CampaignList data={data} />}
-        />
+        <Route path={"/campaigns/list"} element={<CampaignList />} />
         <Route path={"/campaigns/add"} element={<AddCampaign />} />
         <Route
           path={"/campaigns/view/:campaignId"}
