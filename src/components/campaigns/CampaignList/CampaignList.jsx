25e9d5693653570d454df8087de85d4a38a1cd33
bartlomiej <bartlomiejmazur24@gmail.com>
2023-03-05 21:15:14 +0100

    Feat: Create filter function & handle delete btn.

--- src/components/campaigns/CampaignList/CampaignList.jsx
+++ src/components/campaigns/CampaignList/CampaignList.jsx
@@ -8,18 +8,25 @@ import { Button } from "../../reuseComponent/Button";
 import { Input, FormGroup } from "../../reuseComponent/Form";
 import { Container } from "../../reuseComponent/Container";
 import { Flexbox } from "../../reuseComponent/Flexbox";
-import { title, spinner } from "./CampaignList.style";
+import {
+  title,
+  spinner,
+  search_title,
+  add_section,
+  item,
+} from "./CampaignList.style";
 import { Spinner } from "../../reuseComponent/Spinner";
 import { FaSearch, FaPlus } from "react-icons/fa";
 
 import { db } from "../../../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc, query } from "firebase/firestore";
 
 const CampaignList = () => {
-  const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState({ data: [], fillterData: [] });
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState("");
-
+  const [active, setActive] = React.useState(false);
+  const [query, setQuery] = React.useState({ text: "" });
   const dataCollectionRef = collection(db, "campaigns");
 
   React.useEffect(() => {
@@ -27,44 +34,74 @@ const CampaignList = () => {
       try {
         setLoading(true);
         const dataDb = await getDocs(dataCollectionRef);
-        setData(dataDb.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setData({
+          data: dataDb.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
+          fillterData: dataDb.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          })),
+        });
         setLoading(false);
       } catch (error) {
-        setError(error.message);
+        await setError(error);
       }
     };
 
     getData();
-  }, []);
+  }, [active]);
+
+  const deleteItem = async (id) => {
+    setActive(!active);
+    const docRef = doc(db, "campaigns", id);
+    await deleteDoc(docRef);
+  };
+
+  const searchCampaings = (event) => {
+    setQuery({ ...query, text: event.target.value });
+    const theResults = data.data.filter((el) => {
+      return el.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    setData({
+      ...data,
+      fillterData: theResults,
+    });
+  };
 
   return (
     <>
       <Container>
-        <Flexbox>
+        <Flexbox css={add_section}>
           <h3 css={title}>Campaign Manager</h3>
-          <Button variant="green">
-            <Link
-              css={{ textDecoration: "none", color: "white" }}
-              to={`/campaigns/add`}
-            >
+          <Link to={`/campaigns/add`}>
+            <Button variant="green">
               New <FaPlus />
-            </Link>
-          </Button>
+            </Button>
+          </Link>
         </Flexbox>
       </Container>
 
       <Container>
         <FormGroup>
-          <h3 css={{ textAlign: "center" }}>Search Campaign</h3>
+          <h3 css={search_title}>Search Campaign</h3>
 
-          <Input placeholder="Enter campaing name..." />
-          <Flexbox>
-            <Button>
-              Search <FaSearch />
-            </Button>
-          </Flexbox>
+          <Input
+            placeholder="Enter campaing name..."
+            name="text"
+            value={query.text}
+            onChange={searchCampaings}
+          />
         </FormGroup>
-        {loading ? <Spinner css={spinner} /> : <CampaignItem data={data} />}
+        {loading ? (
+          <Spinner css={spinner} />
+        ) : (
+          <div css={item}>
+            <CampaignItem
+              data={data.data}
+              filterData={data.fillterData}
+              deleteItem={deleteItem}
+            />
+          </div>
+        )}
       </Container>
     </>
   );
